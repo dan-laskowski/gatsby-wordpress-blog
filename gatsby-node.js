2abbb38b76e17fcbbd9a7141974821477c52c3a8
Daniel <daniel.laskowski94@gmail.com>
2021-03-19 18:22:16 +0100

    remove a typo

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,7 +14,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   if (result.errors) {
     reporter.error('There was an error fetching posts', result.errors);
   }
-  const { allWpPost } = result?.data;
+  const { allWpPost } = result.data;
   let template = require.resolve(`./src/templates/WpPost.js`);
 
   allWpPost.nodes.map((post) => {
