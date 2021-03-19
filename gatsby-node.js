58a6529fb33c699a85eba5f11daef99d905da88d
Daniel <daniel.laskowski94@gmail.com>
2021-03-19 18:03:37 +0100

    Add data to post template

---
+++ gatsby-node.js
@@ -0,0 +1,27 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    {
+      allWpPost {
+        nodes {
+          id
+          databaseId
+          title
+          uri
+        }
+      }
+    }
+  `);
+  if (result?.errors) {
+    reporter.error('There was an error fetching posts', result.errors);
+  }
+  const { allWpPost } = result?.data;
+  let template = require.resolve(`./src/templates/WpPost.js`);
+
+  allWpPost.nodes.map((post) => {
+    actions.createPage({
+      path: post.uri,
+      component: template,
+      context: post,
+    });
+  });
+};
---
+++ src/templates/WpPost.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Heading, Link, Text } from '@chakra-ui/react';
+import { Link as GatsbyLink, graphql } from 'gatsby';
+
+const WpPost = ({ data: { wpPost } }) => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Heading as={`h1`} m={4}>
+        {wpPost.title}
+      </Heading>
+      <Text mt={4} mb={10}>
+        <div dangerouslySetInnerHTML={{ __html: wpPost.content }} />
+      </Text>
+      <Link as={GatsbyLink} to="/">{`<< Back to Blog`}</Link>
+    </Container>
+  );
+};
+
+export default WpPost;
+
+export const query = graphql`
+  query PostById($id: String) {
+    wpPost(id: { eq: $id }) {
+      title
+      content
+    }
+  }
+`;
