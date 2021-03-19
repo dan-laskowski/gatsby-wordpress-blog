b35d873eb08e9790b18f6d9cf08932d98ae30670
Daniel <daniel.laskowski94@gmail.com>
2021-03-19 13:19:17 +0100

    add data to blog roll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,42 @@
 import * as React from 'react';
-import { Link as GatsbyLink } from 'gatsby';
+import { Link as GatsbyLink, useStaticQuery, graphql } from 'gatsby';
 import { Container, Stack, Box, Heading, Text, Link } from '@chakra-ui/react';
 
 const HomePage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allWpPost {
+        nodes {
+          id
+          excerpt
+          title
+          uri
+        }
+      }
+    }
+  `);
+  const { allWpPost } = data;
   return (
     <Container maxW="xl" centerContent>
       <Heading as={`h1`} m={5}>
         Gatsby Wordpress Blog
       </Heading>
       <Stack spacing={8}>
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as={`h2`}>Hello World</Heading>
-          <Text mt={4}>
-            <span
-              dangerouslySetInnerHTML={{ __html: '<p>This is an excerpt.</p>' }}
-            />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read more
-          </Link>
-        </Box>
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as={`h2`}>Goodbye Mars</Heading>
-          <Text mt={4}>
-            <span
-              dangerouslySetInnerHTML={{
-                __html: '<p>This is another excerpt.</p>',
-              }}
-            />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read more
-          </Link>
-        </Box>
+        {allWpPost.nodes.map(({ id, title, excerpt, uri }) => (
+          <Box key={id} p={5} shadow="md" borderWidth="1px">
+            <Heading as={`h2`}>{title}</Heading>
+            <Text mt={4}>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: `<p>${excerpt}</p>`,
+                }}
+              />
+            </Text>
+            <Link as={GatsbyLink} to={uri}>
+              Read more
+            </Link>
+          </Box>
+        ))}
       </Stack>
     </Container>
   );
