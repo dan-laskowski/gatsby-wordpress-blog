76f758cb9ef8c88459bf95c67310aef31baef836
Daniel <daniel.laskowski94@gmail.com>
2021-03-23 13:35:22 +0100

    add layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-wordpress`,
       options: {
